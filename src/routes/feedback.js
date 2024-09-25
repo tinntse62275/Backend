71e99bd1e69966eb56c0469c14f38381af39b21b
Trương Quang Quyền <QuangQuyen1305@gmail.com>
2024-09-26 01:37:39 +0700

    Initial Commit

---
+++ src/routes/feedback.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const FeedbackController = require('../controllers/FeedbackController');
+const jwtAuth = require('../midlewares/jwtAuth');
+
+let router = express.Router();
+
+router.get('/list/:product_id', FeedbackController.list);
+
+router.get('/detail/:product_variant_id', jwtAuth, FeedbackController.detail);
+
+router.post('/create', jwtAuth, FeedbackController.create);
+
+router.put('/update', jwtAuth, FeedbackController.update);
+
+module.exports = router;
